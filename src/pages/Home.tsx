e55ffe5f83fb1136347988e78408914b3861ef9c
Guilherme <guilherme.vilela@i476.com.br>
2022-06-26 17:52:59 -0300

    todo finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks([
+      ...tasks,
+      { id: tasks.length + 1, title: newTaskTitle, done: false },
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updateTask = tasks.map((task) => ({ ...task }));
+
+    const taskFounded = updateTask.find((task) => task.id === id);
+
+    if (!taskFounded) {
+      return;
+    }
+
+    taskFounded.done = !taskFounded.done;
+    setTasks(updateTask);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const taskFounded = tasks.filter((task) => task.id !== id);
+
+    setTasks(taskFounded);
   }
 
   return (
@@ -26,18 +40,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: '#EBEBEB',
+  },
+});
